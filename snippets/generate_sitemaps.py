a89ab04aec1c802821b2dd23a7e10a32b72aa037
Justin Davies <juda@microsoft.com>
2018-09-12 10:13:49 +0100

    Go go go!

--- snippets/generate_sitemaps.py
+++ snippets/generate_sitemaps.py
@@ -1,20 +1,29 @@
-# -*- coding: utf-8 -*
+import urllib, json
+import glob
+
+
+# Set the latest Block form the API
+
+url = "http://api.inkl.in/api/inklin/live/0"
+response = urllib.urlopen(url)
+data = json.loads(response.read())
+print "Starting from " + str(data["block_number"])
 
 
 counter = 1
 filename = 1
 line = ""
 
-for x in range(6294717, 1, -1):
+for x in range(int(data["block_number"]), 1, -1):
     line = line + "<url>\n"
     line = line + "<loc>http://inkl.in/"+str(x)+ "</loc>\n" 
-    # line = line = "<changefreq>never</changefreq>"
+    line = line + "<changefreq>never</changefreq>"
     line = line + "</url>\n"
 
     counter=counter+1
-    if counter == 45000:
+    if counter == 50000:
         counter = 1
-
+        print("Writing " + str(filename))
         file = open(str(filename) + ".xml", "w") 
         file.write('<?xml version="1.0" encoding="UTF-8"?>\n')
         file.write('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n')
@@ -24,3 +33,19 @@ for x in range(6294717, 1, -1):
         filename = filename + 1
         line = ""
     
+sitemapindex = '<?xml version="1.0" encoding="UTF-8"?>\n'
+sitemapindex = sitemapindex + '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n'
+
+
+for file in glob.glob('*.xml'):
+    sitemapindex = sitemapindex + '<sitemap>\n'
+    sitemapindex = sitemapindex + '  <loc>http://inkl.in/sitemaps/' + file + '</loc>\n'
+    sitemapindex = sitemapindex + '</sitemap>\n'
+
+sitemapindex = sitemapindex + '</sitemapindex>'
+
+sitemap = open("map.xml", "w") 
+sitemap.write(sitemapindex)
+sitemap.close()
+
+print(sitemapindex)
